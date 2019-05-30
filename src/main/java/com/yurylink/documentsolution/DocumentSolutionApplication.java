1306df0581dee3fbcf8ed59a7ce1eaa3a4f51770
Wesley Almeida <yurylink@hotmail.com>
2019-05-29 22:41:56 -0300

    Initial commit

---
+++ src/main/java/com/yurylink/documentsolution/DocumentSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.yurylink.documentsolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentSolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentSolutionApplication.class, args);
+    }
+
+}
